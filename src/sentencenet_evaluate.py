8c32d1c8b354be401da17a90d3c884d44570e765
LuoHuaqing <luohuaqing@4paradigm.com>
2017-03-31 15:43:17 +0800

    evalute::main

--- src/sentencenet_evaluate.py
+++ src/sentencenet_evaluate.py
@@ -1,4 +1,28 @@
 import numpy as np
+import tensorflow as tf
+import datetime
+
+from model.kim_cnn import KimCNN
+import pretrained_word_embedding
+import data_helper
+
+
+tf.app.flags.DEFINE_integer("top_k", 1, "Select top k candidates (default: 1)")
+
+# Data loading params
+tf.app.flags.DEFINE_string("pretrained_word_embedding_file", "", "Data source for the pretrained word embeddings")
+tf.app.flags.DEFINE_string("train_data_file", "", "Data source for the train data")
+tf.app.flags.DEFINE_string("dev_data_file", "", "Data source for the dev data")
+tf.app.flags.DEFINE_string("model_restore_dir", None,
+                           "Directory containing checkpoints used to restore the model (default None)")
+
+tf.app.flags.DEFINE_integer("sequence_length", 20, "Fixed length of sentence (default: 20)")
+tf.app.flags.DEFINE_integer("sentence_embedding_size", 128, "Dimensionality of the sentence embedding (default: 128)")
+tf.app.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
+tf.app.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
+tf.app.flags.DEFINE_integer("word_embedding_size", 50, "Dimensionality of the word embedding (default: 50)")
+
+FLAGS = tf.app.flags.FLAGS
 
 
 def flatten_sentences(sentence_classes):
@@ -16,13 +40,15 @@ def generate_embeddings(sess, net, sentences):
     return sess.run(net.normalized_sentence_embeddings, feed_dict)
 
 
-def classify(dev_embedding, train_embeddings, train_sentence_classes):
+def classify(dev_embedding, train_embeddings, train_sentence_classes, top_k=1):
     dists_sqr = np.sum(np.square(dev_embedding - train_embeddings), 1)
     index = 0
-    min_min_dist = 10000.
-    min_avg_dist = 10000.
-    min_dist_class_name = ""
-    avg_dist_class_name = ""
+    top_k = max(min(top_k, len(train_sentence_classes)), 1)
+
+    min_min_dists = np.full(top_k, 10000.)
+    min_avg_dists = np.full(top_k, 10000.)
+    min_dist_class_names = np.empty(top_k, 'a128')
+    avg_dist_class_names = np.empty(top_k, 'a128')
     for train_sentence_class in train_sentence_classes:
         min_dist = 1000.
         avg_dist = 0.
@@ -33,16 +59,20 @@ def classify(dev_embedding, train_embeddings, train_sentence_classes):
             avg_dist += dist_sqr
             index += 1
         avg_dist /= len(train_sentence_class.sentences)
-        if min_dist < min_min_dist:
-            min_min_dist = min_dist
-            min_dist_class_name = train_sentence_class.name
-        if avg_dist < min_avg_dist:
-            min_avg_dist = avg_dist
-            avg_dist_class_name = train_sentence_class.name
-    return min_dist_class_name, avg_dist_class_name
+
+        arg_max_index = np.argmax(min_min_dists)
+        if min_dist < min_min_dists[arg_max_index]:
+            min_min_dists[arg_max_index] = min_dist
+            min_dist_class_names[arg_max_index] = train_sentence_class.name
+
+        arg_max_index = np.argmax(min_avg_dists)
+        if avg_dist < min_avg_dists[arg_max_index]:
+            min_avg_dists[arg_max_index] = avg_dist
+            avg_dist_class_names[arg_max_index] = train_sentence_class.name
+    return min_dist_class_names, avg_dist_class_names
 
 
-def evaluate(sess, net, dev_sentence_classes, train_sentence_classes):
+def evaluate(sess, net, dev_sentence_classes, train_sentence_classes, top_k=1):
     # flatten sentences
     dev_flatten_sentences = flatten_sentences(dev_sentence_classes)
     train_flatten_sentences = flatten_sentences(train_sentence_classes)
@@ -56,12 +86,45 @@ def evaluate(sess, net, dev_sentence_classes, train_sentence_classes):
     index = 0
     for dev_sentence_class in dev_sentence_classes:
         for i in xrange(len(dev_sentence_class.sentences)):
-            min_dist_class_name, avg_dist_class_name = classify(dev_embeddings[index],
-                                                                train_embeddings,
-                                                                train_sentence_classes)
+            min_dist_class_names, avg_dist_class_names = classify(
+                dev_embeddings[index], train_embeddings, train_sentence_classes, top_k)
             index += 1
-            if min_dist_class_name == dev_sentence_class.name:
+            if dev_sentence_class.name in min_dist_class_names:
                 min_pos_num += 1
-            if avg_dist_class_name == dev_sentence_class.name:
+            if dev_sentence_class.name in avg_dist_class_names:
                 avg_pos_num += 1
     return float(min_pos_num) / len(dev_flatten_sentences), float(avg_pos_num) / len(dev_flatten_sentences)
+
+
+def main(argv=None):
+    word_number_dict, word_embeddings = pretrained_word_embedding.load_word_embedding(
+        FLAGS.pretrained_word_embedding_file, np.zeros(FLAGS.word_embedding_size))
+    train_sentence_classes = data_helper.load_sentences(FLAGS.train_data_file, word_number_dict, FLAGS.sequence_length)
+    dev_sentence_classes = data_helper.load_sentences(FLAGS.dev_data_file, word_number_dict, FLAGS.sequence_length)
+
+    net = KimCNN(
+        sequence_length=FLAGS.sequence_length,
+        filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
+        num_filters=FLAGS.num_filters,
+        pretrained_word_embeddings=word_embeddings,
+        sentence_embedding_size=FLAGS.sentence_embedding_size,
+        word_embedding_static=True)
+
+    sess = tf.Session()
+    saver = tf.train.Saver()
+    if FLAGS.model_restore_dir is None:
+        # Initialize all variables
+        sess.run(tf.global_variables_initializer())
+        sess.run(net.word_embedding_init)
+    else:
+        checkpoint_file_path = tf.train.latest_checkpoint(FLAGS.model_restore_dir)
+        saver.restore(sess, checkpoint_file_path)
+
+    min_accuracy, avg_accuracy = evaluate(sess, net, dev_sentence_classes, train_sentence_classes, FLAGS.top_k)
+    time_str = datetime.datetime.now().isoformat()
+    print ("\nEvaluation-{}: min_accuracy {:g}, avg_accuracy {:g}".
+           format(time_str, min_accuracy, avg_accuracy))
+    print("")
+
+if __name__ == '__main__':
+    tf.app.run()
